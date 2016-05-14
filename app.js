8761c873d4aa9f8cc0b97b0f5b8283f473227a8d
Pawan Rai <pawandeeprai@gmail.com>
2016-05-14 13:18:20 -0700

    it now searches hashes, implements a debounce function, and tiles the photos in divs

--- app.js
+++ app.js
@@ -1,12 +1,56 @@
 $(document).ready(function(){
-  $("input").click(function(){
-    debugger;
-    $.ajax({
-      url: "https://api.instagram.com/v1/tags/dogs/media/recent?access_token=223904829.1fb234f.8f26219ff30a413d93c7ea118c065e09",
-      type: "GET",
-      // dataType: 'jsonp',
-      success: function(data){
-      }
-    });
-  });
+
+  // $(".search").on("input", function(){
+  //
+    // $("div").remove();
+  //   // findHashTag($(".search").val());
+  //
+  // });
+
+  $(".search").on("input", $.debounce(function(){
+    $("div").remove();
+    findHashTag($(".search").val());
+  }, 150, false))
 });
+
+
+
+function findHashTag(hashtag){
+  $.ajax({
+    url: "https://api.instagram.com/v1/tags/" + hashtag + "/media/recent?access_token=223904829.1fb234f.8f26219ff30a413d93c7ea118c065e09",
+    type: "GET",
+    dataType: 'jsonp',
+    success: function(photos){
+      photos.data.forEach(function(photo){
+        var photoUrl = photo.images.standard_resolution.url;
+        var likes = photo.likes.count;
+        var id = photo.id
+        $("body").append("<div class='image' id="+ id +"></div>")
+        $("#" + id).css("background-image", "url("+ photoUrl + ")")
+      });
+    }
+  });
+}
+
+// throttle
+// dbounce
+
+$.debounce = function(func, wait, immediate) {
+    var timeout;
+    return function() {
+        var context = this,
+            args = arguments;
+        var later = function() {
+            timeout = null;
+            if ( !immediate ) {
+                func.apply(context, args);
+            }
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait || 200);
+        if ( callNow ) {
+            func.apply(context, args);
+        }
+    };
+};;
